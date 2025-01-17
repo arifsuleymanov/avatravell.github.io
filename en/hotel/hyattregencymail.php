7305e11b7c056e7e28da94cc23fdc5caac4add28
ARIF SULEYMANOV <005338@ibar.int>
2025-01-18 00:47:44 +0400

    feature/AVA-1 : added en

---
+++ en/hotel/hyattregencymail.php
@@ -0,0 +1,30 @@
+<?php 
+	if(isset($_POST["submit"])){
+		$user_name = $_POST['user_name'];
+		$user_surname = $_POST['user_surname'];
+		$user_email = $_POST['user_email'];
+		$user_phone = $_POST['user_phone'];
+		$user_tour = $_POST['user_tour'];
+		$user_checkin = $_POST['user_checkin'];
+		$user_checkout = $_POST['user_checkout'];
+		$user_adults = $_POST['user_adults'];
+		$user_children7 = $_POST['user_children7'];
+		$user_children0 = $_POST['user_children0'];
+		$user_message = $_POST['user_message'];
+		$to = 'avatravelcompany@gmail.com';
+		$subject = "Hyatt Regency Hotel";
+
+		$message = "Hello my name is ".$user_name." ".$user_surname.".";
+		$message .= "My phone is ".$user_phone.".";
+		$message .= "My email is ".$user_email.". ";
+		$message .= "My tour is ".$user_tour.". ";
+		$message .= "My checkin is ".$user_checkin.". ";
+		$message .= "My checkout is ".$user_checkout.". ";
+		$message .= "Number of Adults ".$user_adults.". ";
+		$message .= "Number of Children between 7 and 11 years: ".$user_children7.". ";
+		$message .= "Number of Children up 7 years: ".$user_children0.". ";
+		$message .= "Message: ".$user_message.". ";
+		mail($to, $subject, $message);
+	}
+	header('Location: /');
+?>	
---
+++ en/shahdagtourmail.php
@@ -0,0 +1,32 @@
+<?php 
+	if(isset($_POST["submit"])){
+		$user_name = $_POST['user_name'];
+		$user_surname = $_POST['user_surname'];
+		$user_email = $_POST['user_email'];
+		$user_phone = $_POST['user_phone'];
+		$user_baku_hotel = $_POST['user_baku_hotel'];
+		$user_qubaqusar_hotel = $_POST['user_qubaqusar_hotel'];
+		$user_checkin = $_POST['user_checkin'];
+		$user_checkout = $_POST['user_checkout'];
+		$user_adults = $_POST['user_adults'];
+		$user_children7 = $_POST['user_children7'];
+		$user_children0 = $_POST['user_children0'];
+		$user_message = $_POST['user_message'];
+		$to = 'avatravelcompany@gmail.com';
+		$subject = "Shahdag Tour";
+
+		$message = "Hello my name is ".$user_name." ".$user_surname.".";
+		$message .= "My phone is ".$user_phone.".";
+		$message .= "My email is ".$user_email.". ";
+		$message .= "My hotel in Baku is ".$user_baku_hotel.". ";
+		$message .= "My hotel in Guba is ".$user_qubaqusar_hotel.". ";
+		$message .= "My checkin is ".$user_checkin.". ";
+		$message .= "My checkout is ".$user_checkout.". ";
+		$message .= "Number of Adults ".$user_adults.". ";
+		$message .= "Number of Children between 7 and 11 years: ".$user_children7.". ";
+		$message .= "Number of Children up 7 years: ".$user_children0.". ";
+		$message .= "Message: ".$user_message.". ";
+		mail($to, $subject, $message);
+	}
+	header('Location: /');
+?>	
